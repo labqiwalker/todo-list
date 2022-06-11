f55930c7f17cd12081ac6b45af354cb22b43dd6e
labqiwalker <labqiwalker@gmail.com>
2022-06-11 06:50:53 -0500

    fix

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { TestPage } from "../pages/TestPage/TestPage";
 import { TodoPage } from "../pages/TodoPage/TodoPage";
 
 export const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<TodoPage />} />
-      <Route path="/test" element={<TestPage />} />
+
     </Routes>
   );
 };
