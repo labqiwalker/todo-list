f2b5cb9eeb01a39ae5b3c5e419de3bf581dc28ff
labqiwalker <labqiwalker@gmail.com>
2022-06-11 06:27:12 -0500

    delete page Test

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { TestPage } from "../pages/TestPage/TestPage";
 import { TodoPage } from "../pages/TodoPage/TodoPage";
 
 export const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<TodoPage />} />
-      <Route path="/test" element={<TestPage />} />
     </Routes>
   );
 };
